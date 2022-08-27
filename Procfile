6e9ed971deb1dd6e1a0930500c57e2b168e39348
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-08-27 12:45:39 +0100

    setting up celery on heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn newsproject.wsgi --log-file -
+worker: celery -A newsapp worker -l info
+beat: celery -A newsapp beat -l info
--- newsapp/celery.py
+++ newsapp/celery.py
@@ -1,8 +1,9 @@
 import os
-
+from django.conf import settings
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newsproject.settings")
 app = Celery("newsapp")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
+#app.conf.update(BROKER_URL=os.getenv('CELERY_BROKER_URL'), CELERY_RESULT_BACKEND=os.getenv('CELERY_RESULT_BACKEND'))
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
